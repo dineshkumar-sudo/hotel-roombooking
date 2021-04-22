3fbc72e5db86eb021b9e7ba07a35e28aa76d4024
Dinesh kumar.S <68317545+dineshkumar-sudo@users.noreply.github.com>
2021-04-22 11:55:36 +0530

    Add files via upload

---
+++ js/hotels.js
@@ -0,0 +1,28 @@
+let hotels = [
+    {
+      "name": "LE MERIDIEN",
+      "type": "7 star",
+      "address": "Le Méridien Coimbatore, Avinashi Road, Cexus Nagar, Neelambur, Tamil Nadu",
+      "ratings": 5
+    },
+    {
+      "name": "Sri Salthi",
+      "type": "5 star",
+      "address": "Sri Sakthi Cinemas, Union Mill Road, KPN Colony, Valipalayam, Tiruppur, Tamil Nadu",
+      "ratings": 4
+    },
+    {
+        "name": "Sterling",
+        "type": "4 star",
+        "address": "Resorts and Hotels, 6/301, Vadakottathara, Kottathara",
+        "ratings": 4
+    },
+    {
+        "name": "Taj",
+        "type": "5 star",
+        "address": "Taj Hotels, KPN Travels, Mettur Road, Municipal Colony, Muncipal Colony, Near, Erode, Tamil Nadu",
+        "ratings": 5
+    }
+  ]
+
+  console.log(hotels);
